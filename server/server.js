a69f84858a794bfaa91e66d7aac2a15466db730e
Student <student@Admins-iMac.local>
2019-01-30 15:54:07 -0600

    Update. Server runs. Sends info to console. Sends info and JSON. Complete for now.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const port = '3000';
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const port = "3000";
 
 let app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/sendName', (req, res) => {
-console.log(req.body.sendName)
-res.send("I'll try to remember that!");
+app.post("/sendName", (req, res) => {
+  console.log(req.body.name);
+  res.send("I'll try to remember that!");
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/looks');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/looks");
 });
 
-app.use(express.static(path.join(__dirname, '/looks')));
-app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+app.use(express.static(path.join(__dirname, "/looks")));
+app.listen(port, function() {
+  console.log("Server listening on port " + port);
+});
+
+const data = new Uint8Array(Buffer.from('Hello Node.js'));
+fs.writeFile('message.txt', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
 });
